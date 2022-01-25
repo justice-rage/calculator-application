3132e09bfbfed18371ba63f1806302c115d2dc1d
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-24 19:52:34 -0800

    add addition business and ui logic
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 function add(number1, number2) {
 	return number1 + number2;
 }
@@ -13,7 +15,14 @@ function subtract(number1, number2) {
 function divide(number1, number2) {
 	return number1 / number2;
 }
-const number1 = parseInt(prompt("Enter a number;"));
-const number2 = parseInt(prompt("Enter a number:"));
-const result = divide(number1, number2);
-alert(result);
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
