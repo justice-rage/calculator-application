ecfa38268820190b9c2828655611e6ec8cdf87a7
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-27 07:56:58 -0800

    add operator selection logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,39 @@
-// Business Logic
+// business logic
 
 function add(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 }
 
-function multiply(number3, number4) {
-	return number3 * number4;
+function subtract(number1, number2) {
+  return number1 - number2;
 }
 
-function subtract(number5, number6) {
-	return number5 - number6;
+function multiply(number1, number2) {
+  return number1 * number2;
 }
 
-function divide(number7, number8) {
-	return number7 / number8;
+function divide(number1, number2) {
+  return number1 / number2;
 }
 
-// User Interface Logic
+// user interface logic
+
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output1").text(result);
-	});
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    const number3 = parseInt($("#multiply1").val());
-    const number4 = parseInt($("#multiply2").val());
-    const result = multiply(number3, number4);
-    $("#output2").text(result);
-  });
-	$("form#subtract").submit(function(event) {
-    event.preventDefault();
-    const number5 = parseInt($("#subtract1").val());
-    const number6 = parseInt($("#subtract2").val());
-    const result = subtract(number5, number6);
-    $("#output3").text(result);
-  });
-	$("form#divide").submit(function(event) {
-    event.preventDefault();
-    const number7 = parseInt($("#divide1").val());
-    const number8 = parseInt($("#divide2").val());
-    const result = divide(number7, number8);
-    $("#output4").text(result);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
